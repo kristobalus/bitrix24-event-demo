0f81f2d128011c416e349799701ec92edbd2e209
ildar <ildar.tamendarov@gmail.com>
2023-02-09 11:47:17 +0600

    update

--- server.ts
+++ server.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import Logger = require("bunyan");
-const log: Logger = Logger.createLogger({name: "demo"});
+const logger: Logger = Logger.createLogger({name: "demo"});
 
 const bodyParser = require('body-parser')
 const express = require('express')
@@ -9,15 +9,15 @@ app.use(bodyParser.json({ type: 'application/*+json' }))
 const port = 80
 
 app.post('/on-deal-changed', (req: Request, res: Response) => {
-    log.info(req.body)
+    logger.info(req.body)
     res.status(200).send()
 })
 
 app.post('/on-lead-changed', (req: Request, res: Response) => {
-    log.info(req.body)
+    logger.info(req.body)
     res.status(200).send()
 })
 
 app.listen(port, () => {
-    log.info(`Demo app listening on port ${port}`)
+    logger.info(`Demo app listening on port ${port}`)
 })
